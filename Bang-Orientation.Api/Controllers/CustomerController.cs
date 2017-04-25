7ea40c50f162f3e41ceb95c49e357466824c542b
Andi DeVois <adevois@gmail.com>
2017-04-25 18:50:53 -0500

    Resolved packages, added intial classes

---
+++ Bang-Orientation.Api/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Bang_Orientation.Api.Models;
+
+namespace Bang_Orientation.Api.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public HttpResponseMessage RegisterCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
