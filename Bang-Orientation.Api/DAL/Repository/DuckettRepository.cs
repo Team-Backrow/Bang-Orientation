651f440a09b1e6ae947056fa54b35b59465249d4
blairvansant <blairbot@gmail.com>
2017-04-25 19:11:38 -0500

    add duckett class, repo, and interface

---
+++ Bang-Orientation.Api/DAL/Repository/DuckettRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bang_Orientation.Api.DAL.Repository
+{
+    public class DuckettRepository
+    {
+    }
+}
